20235dd32025865a0c50bbdf8b7962a53e53ad29
cjh <chenwx58668@163.com>
2024-06-07 16:17:52 +0800

    first commit

---
+++ src/components/MyDivider.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "MyDevider"
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/plugins/myAxios.ts
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "myAxios"
+}
+</script>
+
+<style scoped>
+
+</style>
